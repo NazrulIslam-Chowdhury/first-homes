fef69cf221bebdfa23346558f4f334cf6ebd8954
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-11-13 12:18:20 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.hnlrj23.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
