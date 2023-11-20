a6a76707828b6ff17e3b48ccaa5ddff71cc2302a
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-11-20 09:57:36 +0600

    create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
@@ -31,6 +32,7 @@ app.listen(port, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
