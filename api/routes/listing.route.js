494164e4ee5f1c997e69c8e2472e89c88e769ec3
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-11-24 12:03:24 +0600

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -13,5 +14,6 @@ listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
 listingRouter.post("/update/:id", verifyToken, updateListing);
 listingRouter.get("/get/:id", getListing);
+listingRouter.get("/get", getListings);
 
 export default listingRouter;
