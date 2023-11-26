0ab3e255d55186ab6813b5804e7f1cefac059cf9
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-11-26 11:52:05 +0600

    .....

--- api/index.js
+++ api/index.js
@@ -7,14 +7,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import path from "path";
-const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
-
-// middleware
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
 
 mongoose
   .connect(
@@ -28,6 +22,12 @@ mongoose
   });
 
 const __dirname = path.resolve();
+
+const app = express();
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
