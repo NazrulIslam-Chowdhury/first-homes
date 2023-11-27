a4380eea98587d17274a12d05fbcef6643940c27
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-11-27 12:05:50 +0600

    .....

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://first-homes-server.vercel.app",
+        target: "http://localhost:5000",
         secure: false,
       },
     },
