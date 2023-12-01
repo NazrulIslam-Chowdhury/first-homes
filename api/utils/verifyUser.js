c667f0a3ba4cd5d94a28edc8d116289b85aa8266
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-12-01 19:15:10 +0600

    verify user

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,10 +1,15 @@
+import { signOut } from "../controllers/auth.controller.js";
 import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) return next(errorHandler(401, "Unauthorized"));
+  if (!token) {
+    next(errorHandler(401, "Unauthorized"));
+    signOut();
+    return;
+  }
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
