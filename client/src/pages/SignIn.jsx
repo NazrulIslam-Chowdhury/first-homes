52113032754f5831e8db6ea54c549a1e0cffb861
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-12-02 17:58:24 +0600

    sign in page ui changed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -47,7 +47,10 @@ const SignIn = () => {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <form
+        className="flex flex-col gap-4 bg-[#cddcbc] p-7 rounded-lg"
+        onSubmit={handleSubmit}
+      >
         <input
           type="email"
           placeholder="email"
