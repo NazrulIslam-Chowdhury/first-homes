69f81ef98406981d564499412533d2204e28c42e
NazrulIslam-Chowdhury <nazrulislam66188@gmail.com>
2023-12-03 18:54:54 +0600

    updating listing page ui changed

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -162,7 +162,7 @@ const UpdateListing = () => {
   };
   return (
     <main className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">
+      <h1 className="text-3xl font-semibold text-center my-7 uppercase">
         Update a Listing
       </h1>
       <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-6">
@@ -170,7 +170,7 @@ const UpdateListing = () => {
           <input
             type="text"
             placeholder="Name"
-            className="border p-3 rounded-lg"
+            className="border p-3 rounded-lg outline-lime-400"
             id="name"
             maxLength="62"
             minLength="10"
@@ -181,7 +181,7 @@ const UpdateListing = () => {
           <textarea
             type="text"
             placeholder="Description"
-            className="border p-3 rounded-lg"
+            className="border p-3 rounded-lg outline-lime-400"
             id="description"
             rows="5"
             onChange={handleChange}
@@ -191,13 +191,13 @@ const UpdateListing = () => {
           <input
             type="text"
             placeholder="Address"
-            className="border p-3 rounded-lg"
+            className="border p-3 rounded-lg outline-lime-400"
             id="address"
             onChange={handleChange}
             value={formData.address}
             required
           />
-          <div className="flex gap-6 flex-wrap mt-5">
+          <div className="flex gap-6 flex-wrap mt-5 accent-lime-400">
             <div className="flex gap-2">
               <input
                 type="checkbox"
@@ -258,7 +258,7 @@ const UpdateListing = () => {
                 max="10"
                 onChange={handleChange}
                 value={formData.bedrooms}
-                className="p-3 border border-gray-300 rounded-lg"
+                className="p-3 border border-gray-300 rounded-lg outline-lime-400"
                 required
               />
               <span>Beds</span>
@@ -271,7 +271,7 @@ const UpdateListing = () => {
                 max="10"
                 onChange={handleChange}
                 value={formData.bathrooms}
-                className="p-3 border border-gray-300 rounded-lg"
+                className="p-3 border border-gray-300 rounded-lg outline-lime-400"
                 required
               />
               <span>Baths</span>
@@ -284,7 +284,7 @@ const UpdateListing = () => {
                 max="10000000"
                 onChange={handleChange}
                 value={formData.regularPrice}
-                className="p-3 border border-gray-300 rounded-lg"
+                className="p-3 border border-gray-300 rounded-lg outline-lime-400"
                 required
               />
               <div className="flex flex-col items-center">
@@ -301,7 +301,7 @@ const UpdateListing = () => {
                   max="10000000"
                   onChange={handleChange}
                   value={formData.discountPrice}
-                  className="p-3 border border-gray-300 rounded-lg"
+                  className="p-3 border border-gray-300 rounded-lg outline-lime-400"
                   required
                 />
                 <div className="flex flex-col items-center">
@@ -331,7 +331,7 @@ const UpdateListing = () => {
             <button
               type="button"
               disabled={uploading}
-              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+              className="p-3 text-white font-semibold border bg-[#AECF75] rounded uppercase hover:shadow-lg hover:bg-[#9ec45b] disabled:opacity-80"
               onClick={handleImgUpload}
             >
               {uploading ? "uploading...." : "upload"}
